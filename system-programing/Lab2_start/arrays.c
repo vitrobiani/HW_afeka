aacb97e6264db3d62341fe6b66eac2a7d887f7b5
vitrobiani <vitrobiani@gmail.com>
2023-10-16 14:17:01 +0300

    doing the arrays.c

--- system-programing/Lab2_start/arrays.c
+++ system-programing/Lab2_start/arrays.c
@@ -1,2 +1,38 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
+
+void printArr(int arr[], int len){
+    for (int i = 0; i < len; i++) {
+        printf("%d ", arr[i]);
+    }
+}
+
+int* init_arr(int len){
+    int* arr = malloc(sizeof(int)*len);
+    return arr;
+}
+
+int** init_mat(int col, int row){
+    int** mat = malloc(sizeof(int*)*row);
+    for (int i = 0; i < row; i++) {
+        mat[i] = malloc(sizeof(int)*col);
+        for (int j = 0; j < col; j++) {
+            mat[i][j] = rand()%50;
+        }
+    }
+    
+    return mat;
+}
+
+void printMat(int** mat, int row, int col)
+{
+    for (int i = 0; i < row; i++) {
+        //int len = sizeof(mat[i])/sizeof(mat[i][0]);
+        for (int j = 0; j < col; j++) {
+            printf("%3d ", mat[i][j]); 
+        }
+        printf("\n");
+    }
+}
+
--- system-programing/Lab2_start/arrays.h
+++ system-programing/Lab2_start/arrays.h
@@ -1,9 +1,20 @@
-
-  
+#ifndef ARRAYS
+#define ARRAYS
 
 //void	printArray
+
+void printArr(int arr[], int len);
+
 //void	initArray
+
+int* init_arr(int len);
+
 //void	printMat
+
+void printMat(int** mat, int row, int col);
+
 //void	initMat
 
+int** init_mat(int col, int row);
 
+#endif // !ARRAYS
